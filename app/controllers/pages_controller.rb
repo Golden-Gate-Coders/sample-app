eb62f091c2e47c65f5e82b26cbd8ac291c4c9f53
Danish Mughal <mughal.danish@gmail.com>
2017-03-06 19:44:11 -0800

    Instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,23 @@
 class PagesController < ApplicationController
 
   def hello_method
+    @title = "Hello!"
+    @current_time = Time.now.strftime("%A, %b %d")
+
+    num_1 = 1
+    num_2 = 2
+    @sum = num_1 + num_2
+
     render 'hello.html.erb'
+  end
+
+  def contact_method
+    @title = "Contact Us"
+    render 'contact.html.erb'
+  end
+
+  def about
+    @title = "About Us"
   end
 
 end
